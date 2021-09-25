a2118a3e4e1610e9671fb9533c09ba243000525e
Даня Дуров <danyadurovnn@gmail.com>
2021-09-25 21:36:33 +0300

    Частичное уничтожение констукций if else

--- api.eviger.ru/index.php
+++ api.eviger.ru/index.php
@@ -122,44 +122,27 @@ switch ($method) {
             switch ($mixedData['method']) {
 
                 case 'auth':
-                    if (isset($mixedData['login']) && (mb_strlen($mixedData['login']) > 6 && mb_strlen($mixedData['login']) < 20) && preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login']) && Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s'", $mixedData['login'])->getNumRows()) {
+                    // login checks
 
-                        if (isset($mixedData['password']) && (mb_strlen($mixedData['password']) > 8 && mb_strlen($mixedData['password']) < 60) && preg_match("/[a-zA-Z0-9_]/ui", $mixedData['password'])) {
+                    if (!isset($mixedData['login'])) die(Other::generateJson(["response" => ["error" => "login not setted"]]));
 
-                            User::auth($mixedData['login'], $mixedData['password']);
+                    if (mb_strlen($mixedData['login']) <= 6 || mb_strlen($mixedData['login']) >= 20) die(Other::generateJson(["response" => ["error" => "the login is too big or too small"]]));
 
-                        } elseif (!isset($mixedData['password'])) {
+                    if (!preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login'])) die(Other::generateJson(["response" => ["error" => "the login must contain a-z, A-Z, 0-9 and _"]]));
 
-                            die(Other::generateJson(["response" => ["error" => "password not setted"]]));
+                    if (!Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s'", $mixedData['login'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "user not found"]]));
 
-                        } elseif (isset($mixedData['password']) && (mb_strlen($mixedData['password']) <= 8 || $mixedData['password'] >= 60)) {
+                    // password checks
 
-                            die(Other::generateJson(["response" => ["error" => "the password is too big or too small"]]));
+                    if (!isset($mixedData['password'])) die(Other::generateJson(["response" => ["error" => "password not setted"]]));
 
-                        } elseif (isset($mixedData['password']) && (mb_strlen($mixedData['password']) > 8 && $mixedData['password'] < 60) && !preg_match("/[a-zA-Z0-9_]/ui", $mixedData['password'])) {
+                    if ((mb_strlen($mixedData['password']) <= 8 || $mixedData['password'] >= 60)) die(Other::generateJson(["response" => ["error" => "the password is too big or too small"]]));
 
-                            die(Other::generateJson(["response" => ["error" => "the password must contain a-z, A-Z, 0-9 and _"]]));
+                    if (!preg_match("/[a-zA-Z0-9_]/ui", $mixedData['password'])) die(Other::generateJson(["response" => ["error" => "the password must contain a-z, A-Z, 0-9 and _"]]));
 
-                        }
+                    // processing authentication
 
-                    } elseif (!isset($mixedData['login'])) {
-
-                        die(Other::generateJson(["response" => ["error" => "login not setted"]]));
-
-                    } elseif (isset($mixedData['login']) && (mb_strlen($mixedData['login']) <= 6 || mb_strlen($mixedData['login']) >= 20)) {
-
-                        die(Other::generateJson(["response" => ["error" => "the login is too big or too small"]]));
-
-                    } elseif (isset($mixedData['login']) && (mb_strlen($mixedData['login']) > 6 && mb_strlen($mixedData['login']) < 20) && !preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login'])) {
-
-                        die(Other::generateJson(["response" => ["error" => "the login must contain a-z, A-Z, 0-9 and _"]]));
-
-                    } elseif (isset($mixedData['login']) && (mb_strlen($mixedData['login']) > 6 && mb_strlen($mixedData['login']) < 20) && preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login']) && !Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s'", $mixedData['login'])->getNumRows()) {
-
-                        die(Other::generateJson(["response" => ["error" => "the user not found"]]));
-
-                    }
-                    break;
+                    die(User::auth($mixedData['login'], $mixedData['password']));
 
                 default:
                     die(Other::generateJson(["response" => ["error" => "unknown sub-method", "parameters" => $mixedData]]));
@@ -168,84 +151,62 @@ switch ($method) {
         } else {
             switch ($mixedData['method']) {
                 case 'registerAccount':
+                    // login checks
 
-                    if (isset($mixedData['login']) && (mb_strlen($mixedData['login']) > 6 && mb_strlen($mixedData['login']) < 20) && preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login']) && !Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s'", $mixedData['login'])->getNumRows()) {
+                    if (!isset($mixedData['login'])) die(Other::generateJson(["response" => ["error" => "login not setted"]]));
 
-                        if (isset($mixedData['password']) && (mb_strlen($mixedData['password']) > 8 && mb_strlen($mixedData['password']) < 60) && preg_match("/[a-zA-Z0-9_]/ui", $mixedData['password'])) {
+                    if (mb_strlen($mixedData['login']) <= 6 || mb_strlen($mixedData['login']) >= 20) die(Other::generateJson(["response" => ["error" => "the login is too big or too small"]]));
 
-                            if (isset($mixedData['email']) && preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $mixedData['email']) && !Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE email = '?s'", $mixedData['email'])->getNumRows()) {
+                    if (!preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login'])) die(Other::generateJson(["response" => ["error" => "the login must contain a-z, A-Z, 0-9 and _"]]));
 
-                                if ($mixedData['userName'] !== null && isset($mixedData['userName'])) {
+                    if (Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s'", $mixedData['login'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "the user is already registered"]]));
 
-                                    if (preg_match("/^(e)?id.*/gu", $mixedData['userName'])) die(Other::generateJson(["response" => ["error" => "username cannot contain the prefix eid or id"]]));
+                    // password checks
 
-                                    if (Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE username = '?s'", $mixedData['userName'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "username is busy"]]));
+                    if (!isset($mixedData['password'])) die(Other::generateJson(["response" => ["error" => "password not setted"]]));
 
-                                }
+                    if ((mb_strlen($mixedData['password']) <= 8 || $mixedData['password'] >= 60)) die(Other::generateJson(["response" => ["error" => "the password is too big or too small"]]));
 
-                                if (isset($mixedData['registrationEmailCode'])) {
+                    if (!preg_match("/[a-zA-Z0-9_]/ui", $mixedData['password'])) die(Other::generateJson(["response" => ["error" => "the password must contain a-z, A-Z, 0-9 and _"]]));
 
-                                    die(User::registerAccount($mixedData['login'], $mixedData['password'], $mixedData['email'], ($mixedData['userName'] === null) ? null : $mixedData['userName'], $mixedData['registrationEmailCode'], $mixedData['hashCode']));
+                    // email checks
 
-                                } else {
+                    if (!isset($mixedData['email'])) die(Other::generateJson(["response" => ["error" => "email not setted"]]));
 
-                                    $getCode = json_decode(Email::createCode($mixedData['email'], Mail::getInstance()), true);
+                    if (!preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $mixedData['email'])) die(Other::generateJson(["response" => ["error" => "email incorrect"]]));
 
-                                    if (isset($getCode['response']['error'])) {
-                                        die(Other::generateJson(["response" => ["error" => $getCode['response']['error']]]));
-                                    } else {
-                                        die(Other::generateJson(["response" => ["status" => "confirm your email", "hash" => $getCode['response']["hash"]]]));
-                                    }
+                    if (Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE email = '?s'", $mixedData['email'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "email is busy"]]));
 
-                                }
+                    // username checks
 
-                            } elseif (!isset($mixedData['email'])) {
+                    if ($mixedData['userName'] !== null && isset($mixedData['userName'])) {
 
-                                die(Other::generateJson(["response" => ["error" => "email not setted"]]));
+                        if (mb_strlen($mixedData['userName']) < 6 || mb_strlen($mixedData['userName']) > 128) die(Other::generateJson(["response" => ["error" => "username must be more than 6 or less than 128 characters"]]));
 
-                            } elseif (isset($mixedData['email']) && !preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $mixedData['email'])) {
+                        if (preg_match("/^(e)?id.*/gu", $mixedData['userName'])) die(Other::generateJson(["response" => ["error" => "username cannot contain the prefix eid or id"]]));
 
-                                die(Other::generateJson(["response" => ["error" => "email incorrect"]]));
-
-                            } elseif (isset($mixedData['email']) && preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $mixedData['email']) && Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE email = '?s'", $mixedData['email'])->getNumRows()) {
-
-                                die(Other::generateJson(["response" => ["error" => "email is busy"]]));
-
-                            }
-
-                        } elseif (!isset($mixedData['password'])) {
-
-                            die(Other::generateJson(["response" => ["error" => "password not setted"]]));
-
-                        } elseif (isset($mixedData['password']) && (mb_strlen($mixedData['password']) < 8 || mb_strlen($mixedData['password']) > 60)) {
-
-                            die(Other::generateJson(["response" => ["error" => "the password is too big or too small"]]));
-
-                        } elseif (isset($mixedData['password']) && (mb_strlen($mixedData['password']) > 8 && mb_strlen($mixedData['password']) < 60) && !preg_match("/[a-zA-Z0-9_]/ui", $mixedData['password'])) {
-
-                            die(Other::generateJson(["response" => ["error" => "the password must contain a-z, A-Z, 0-9 and _"]]));
-
-                        }
-
-                    } elseif (!isset($mixedData['login'])) {
-
-                        die(Other::generateJson(["response" => ["error" => "login not setted"]]));
-
-                    } elseif (isset($mixedData['login']) && (mb_strlen($mixedData['login']) <= 6 || mb_strlen($mixedData['login']) >= 20)) {
-
-                        die(Other::generateJson(["response" => ["error" => "the login is too big or too small"]]));
-
-                    } elseif (isset($mixedData['login']) && (mb_strlen($mixedData['login']) > 6 && mb_strlen($mixedData['login']) < 20) && !preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login'])) {
-
-                        die(Other::generateJson(["response" => ["error" => "the login must contain a-z, A-Z, 0-9 and _"]]));
-
-                    } elseif (isset($mixedData['login']) && (mb_strlen($mixedData['login']) > 6 && mb_strlen($mixedData['login']) < 20) && preg_match("/[a-zA-Z0-9_]/ui", $mixedData['login']) && Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s'", $mixedData['login'])->getNumRows()) {
-
-                        die(Other::generateJson(["response" => ["error" => "the user is already registered"]]));
+                        if (Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE username = '?s'", $mixedData['userName'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "username is busy"]]));
 
                     }
 
-                    break;
+                    // processing registering account or email confirmation
+
+                    if (isset($mixedData['registrationEmailCode'])) {
+
+                        die(User::registerAccount($mixedData['login'], $mixedData['password'], $mixedData['email'], ($mixedData['userName'] === null) ? null : $mixedData['userName'], $mixedData['registrationEmailCode'], $mixedData['hashCode']));
+
+                    } else {
+
+                        $getCode = json_decode(Email::createCode($mixedData['email'], Mail::getInstance()), true);
+
+                        if (isset($getCode['response']['error'])) {
+                            die(Other::generateJson(["response" => ["error" => $getCode['response']['error']]]));
+                        } else {
+                            die(Other::generateJson(["response" => ["status" => "confirm your email", "hash" => $getCode['response']["hash"]]]));
+                        }
+
+                    }
+
                 case 'restorePassword':
                     if (Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE login = '?s' AND email = '?s'", $mixedData['login'], $mixedData['email'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "login/email pair is not correct"]]));
 
@@ -274,16 +235,34 @@ switch ($method) {
                     die(User::setOffline($mixedData['token']));
 
                 case 'changeName':
-                    if (isset($mixedData['name']) && isset($mixedData['email']) && isset($mixedData['code']) && isset($mixedData['hashCode'])) {
-                        die(User::changeName($mixedData['name'], $mixedData['email'], $mixedData['code'], $mixedData['hashCode']));
-                    }
-                    break;
+                    // newName checks
+
+                    if (!isset($mixedData['newName'])) die(Other::generateJson(["response" => ["error" => "newName not setted"]]));
+
+                    if (mb_strlen($mixedData['newName']) < 6 || mb_strlen($mixedData['newName']) > 128) die(Other::generateJson(["response" => ["error" => "newName must be more than 6 or less than 128 characters"]]));
+
+                    if (preg_match("/^(e)?id.*/gu", $mixedData['newName'])) die(Other::generateJson(["response" => ["error" => "newName cannot contain the prefix eid or id"]]));
+
+                    if (Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE username = '?s'", $mixedData['userName'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "username is busy"]]));
+
+                    // email checks
+
+                    if (!isset($mixedData['email'])) die(Other::generateJson(["response" => ["error" => "email not setted"]]));
+
+                    if (!Database::getInstance()->query("SELECT * FROM eviger.eviger_users WHERE email = '?s'", $mixedData['email'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "email not found"]]));
+
+                    // code and hashCode checks
+
+                    if (!isset($mixedData['code']) || !isset($mixedData['hashCode'])) die(Other::generateJson(["response" => ["error" => "hashCode or code not setted"]]));
+
+                    if (!Database::getInstance()->query("SELECT * FROM eviger.eviger_codes_email WHERE code = '?s' AND hash = '?s'", $mixedData['code'], $mixedData['hashCode'])->getNumRows()) die(Other::generateJson(["response" => ["error" => "code or hash invalid"]]));
+
+                    die(User::changeName($mixedData['name'], $mixedData['email'], $mixedData['code'], $mixedData['hashCode']));
 
                 default:
                     die(Other::generateJson(["response" => ["error" => "unknown sub-method", "parameters" => $mixedData === null ? [] : $mixedData]]));
             }
         }
-        break;
     case "users":
         switch ($mixedData['method']) {
 
