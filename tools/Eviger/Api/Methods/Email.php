d5a9e286a793e946ea24b8fa5e4420768d1df738
Даня Дуров <danyadurovnn@gmail.com>
2021-10-06 22:04:24 +0300

    Добавлена проверка на IP-адрес и поправлен текст письма

--- tools/Eviger/Api/Methods/Email.php
+++ tools/Eviger/Api/Methods/Email.php
@@ -22,25 +22,25 @@ class Email
 
             if (!preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $email)) return Other::generateJson(["response" => ["error" => "email incorrect"]]);
 
+            if (preg_match("/(.*)?eviger\.ru/isu", $email)) return Other::generateJson(["response" => ["error" => "email must not contain domains of any level eviger.ru"]]);
+
             $code = mb_substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 16);
             $hash = md5($code . "|" . bin2hex(random_bytes(8)));
 
             if (Database::getInstance()->query("SELECT * FROM eviger.eviger_codes_email WHERE email = '?s'", $email)->getNumRows()) {
-                if ((time() - Database::getInstance()->query("SELECT date_request FROM eviger.eviger_codes_email WHERE email = '?s'", $email)->fetchAssoc()['date_request']) > 300) {
-                    Database::getInstance()->query("UPDATE eviger.eviger_codes_email SET code = '?s', date_request = ?i, hash = '?s' WHERE email = '?s'", $code, time(), $hash, $email);
-                } else {
-                    return Other::generateJson(["response" => ["error" => "cooldown"]]);
-                }
+                if ((time() - Database::getInstance()->query("SELECT date_request FROM eviger.eviger_codes_email WHERE email = '?s'", $email)->fetchAssoc()['date_request']) < 300 || Database::getInstance()->query("SELECT * FROM eviger.eviger_codes_email WHERE ip_request = '?s'", $_SERVER['REMOTE_ADDR'])->getNumRows()) return Other::generateJson(["response" => ["error" => "cooldown"]]);
+
+                Database::getInstance()->query("UPDATE eviger.eviger_codes_email SET code = '?s', date_request = ?i, hash = '?s' WHERE email = '?s'", $code, time(), $hash, $email);
             } else {
                 Database::getInstance()->query("INSERT INTO eviger.eviger_codes_email (code, email, date_request, hash) VALUES ('?s', '?s', ?i, '?s')", $code, $email, time(), $hash);
             }
 
-            $mail->setFrom('user@domain.com');
+            $mail->setFrom(getenv("USER_MAIL_DOMAIN"));
             $mail->addAddress($email);
 
             $mail->isHTML(true);
             $mail->Subject = "Подтверждение почты";
-            $mail->Body = "Ваш код подтверждения: <b>$code</b>";
+            $mail->Body = "Ваш код подтверждения: <b>$code</b>\n\nДанный код будет активен в течении часа с момента получения письма\nЕсли вы не запрашивали данное письмо <b>немендленно смените пароль</b>";
             $mail->CharSet = "UTF-8";
             $mail->Encoding = 'base64';
 
