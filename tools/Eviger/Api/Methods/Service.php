721384e948f7d51b58f404bd22772a475109c626
Даня Дуров <danyadurovnn@gmail.com>
2021-10-06 22:06:05 +0300

    А это как сюда попало

--- tools/Eviger/Api/Methods/Service.php
+++ tools/Eviger/Api/Methods/Service.php
@@ -18,7 +18,7 @@ class Service
                 "version" => Database::getInstance()->query("SELECT version FROM eviger.eviger_updates ORDER BY id DESC LIMIT 1")->fetchAssoc()['version'],
                 "download_link" => "https://" . explode("/var/www/", Database::getInstance()->query("SELECT dl FROM eviger.eviger_updates ORDER BY id DESC LIMIT 1")->fetchAssoc()['dl'])[1],
                 "changelog" => Database::getInstance()->query("SELECT changelog FROM eviger.eviger_updates ORDER BY id DESC LIMIT 1")->fetchAssoc()['changelog']
-            ]
+                ]
             ]
         );
 
